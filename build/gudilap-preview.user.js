da9dc6b11a22a37d20da54621406e58bab993b48
Pavel Shakhov <ab.pongo@gmail.com>
2016-07-06 17:38:41 +0300

    Добавил поддомен www

--- build/gudilap-preview.user.js
+++ build/gudilap-preview.user.js
@@ -3,8 +3,10 @@
 // @description Добавляет предпросмотр на форум Гудилапа
 // @namespace   https://github.com/pongo
 // @include     http://gudilap.ru/commentadd/*
+// @include     http://www.gudilap.ru/commentadd/*
 // @include     http://gudilap.ru/topicadd/*
-// @version     1
+// @include     http://www.gudilap.ru/topicadd/*
+// @version     2
 // @grant       console.log
 // @grant       GM_addStyle
 // @updateURL   https://github.com/pongo/gudilap-preview/raw/master/build/gudilap-preview.meta.js
--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,10 @@ const userscript_header = `
 // @description Добавляет предпросмотр на форум Гудилапа
 // @namespace   https://github.com/pongo
 // @include     http://gudilap.ru/commentadd/*
+// @include     http://www.gudilap.ru/commentadd/*
 // @include     http://gudilap.ru/topicadd/*
-// @version     1
+// @include     http://www.gudilap.ru/topicadd/*
+// @version     2
 // @grant       console.log
 // @grant       GM_addStyle
 // @updateURL   https://github.com/pongo/gudilap-preview/raw/master/build/gudilap-preview.meta.js
